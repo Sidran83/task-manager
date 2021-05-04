a16bd0d2897b6552c516c4e14ca3f291f64a46ee
pierre deveix <deveixpierre@gmail.com>
2021-05-04 15:18:38 +0200

    CRUD added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks' => 'tasks#index'
-  get 'tasks/new' => 'tasks#new'
-  get 'tasks/:id' => 'tasks#show', as: :task
-  post 'tasks' => 'tasks#create'
+  resources :tasks
 end
